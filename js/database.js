775d4d034af206132bd002a51e6b5a98d8825406
Srinivash <srinivashu20@gmail.com>
2026-03-20 10:40:52 +0530

    Update

--- js/database.js
+++ js/database.js
@@ -1,1 +1,38 @@
-
+const products = [
+    {
+        id: 1,
+        name: "Coca Cola",
+        price: 50,
+        image: "https://www.coca-cola.com/content/dam/GB/en_GB/Pages/home-page/hero/coca-cola-glass-bottle.png"
+    },
+    {
+        id: 2,
+        name: "Pepsi",
+        price: 50,
+        image: "https://www.pepsi.com/content/dam/pepsi/us/en/products/pepsi-cola-bottle.png"
+    },
+    {
+        id: 3,
+        name: "Sprite",
+        price: 45,
+        image: "https://www.pepsi.com/content/dam/pepsi/us/en/products/sprite-bottle.png"
+    },
+    {
+        id: 4,
+        name: "Fanta",
+        price: 40,
+        image: "https://www.pepsi.com/content/dam/pepsi/us/en/products/fanta-orange-bottle.png"
+    },
+    {
+        id: 5,
+        name: "Dr Pepper",
+        price: 50,
+        image: "https://www.pepsi.com/content/dam/pepsi/us/en/products/dr-pepper-bottle.png"
+    },
+    {
+        id: 6,
+        name: "Mountain Dew",
+        price: 55,
+        image: "https://www.pepsi.com/content/dam/pepsi/us/en/products/mountain-dew-bottle.png"
+    }
+];
